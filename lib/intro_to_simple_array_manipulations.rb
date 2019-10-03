7826a9b33da705fe98372154a12c065805a5d25d
jules blanc <jules.blanc@unccub.com>
2019-10-03 12:01:27 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array)
+  array.unshift
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift 
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array2.concat(array1) 
+end
+
+def using_insert(array, element)
+  array[3].insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array)
+  array.delete 
+end 
+
+def using_delete_at(array, integer)
+  array[integer].delete
+end
